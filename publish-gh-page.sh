48be5ffbba9cf4b7847d9a176250bdecaca3dc94
Gaëtan Maisse <gaetan.maisse@mythalesgroup.com>
2018-02-19 22:15:42 +0100

    build(): Update publish script to push on master branch

--- publish-gh-page.sh
+++ publish-gh-page.sh
@@ -3,9 +3,9 @@
 cd dist/
 
 git init
-git checkout -b test-gh-page
+git checkout -b master
 git remote add origin	https://github.com/ngx-rocket/ngx-rocket.github.io.git
 
 git add -A
 git commit -m "Auto commit for GH page - Sources are on dev branch"
-git push --set-upstream origin test-gh-page
+git push --set-upstream origin master
