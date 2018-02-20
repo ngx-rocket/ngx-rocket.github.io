6eb6a77ce2c79f9ff004295c4f60d3a92dd5e7aa
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2018-02-20 09:42:48 +0100

    Updated publish script

--- scripts/publish-gh-page.sh
+++ scripts/publish-gh-page.sh
@@ -8,4 +8,4 @@ git remote add origin	https://github.com/ngx-rocket/ngx-rocket.github.io.git
 
 git add -A
 git commit -m "Auto commit for GH page - Sources are on dev branch"
-git push --set-upstream origin master
+git push --set-upstream origin master --force
